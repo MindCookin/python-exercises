b613d97d3c2d604cf016a2fea4a0b64dec79ef09
MindCookin <mindcookin@gmail.com>
2016-05-17 13:17:47 +0200

    Pretty printing

--- chapter02/triangleNumbers.py
+++ chapter02/triangleNumbers.py
@@ -4,11 +4,13 @@
 n = input("Gimme a number: ")
 total = 0
 
-print "Complex equation"
+print """
+Complex equation"""
 for x in range(1, n + 1):
   print "{0} - {1}".format(x, x * (x + 1) / 2)
 
-print "Simple variable assignment"
+print """
+Simple variable assignment"""
 for x in range(1, n + 1):
   total = total + x
   print "{0} - {1}".format(x, total)
