0a0340c5e74e6585b7f6c482be90e3df96952a6e
MindCookin <mindcookin@gmail.com>
2016-05-23 11:09:30 +0200

    Two number multiplication maximum search algorythm optimized

--- chapter03/maxProductOfTwoNumbers.py
+++ chapter03/maxProductOfTwoNumbers.py
@@ -6,29 +6,19 @@ import math
 fourNumbers = input('Write four numbers separated by commas')
 target = []
 
-def multiplyToOthers(index):
+def multiplyToNext(index, maximum):
 
-  results = [4]
+  for x in range(index,4):
+      print ('operation! {0} * {1}'.format(fourNumbers[index],fourNumbers[x]))
+      result = max(fourNumbers[index] * fourNumbers[x], maximum)
 
-  for x in range(0,4):
-    if (index != x):
-      result = fourNumbers[index] * fourNumbers[x]
-      results.insert(x, result)
-    else: 
-      results.insert(x, -1)
-
-  return results
+  return result
 
 i = 0
 currentMax = 0
 
-while (i < 4):
-  
-  ml = multiplyToOthers(i)
-  ml.append(currentMax)
-
-  currentMax = max(ml)
-
+while (i < 3):
+  currentMax = max(multiplyToNext(i, currentMax), currentMax)
   i = i + 1
 
 print 'Maximum multiplication number for two numbers in {0} tuple is {1}'.format(fourNumbers, currentMax)
